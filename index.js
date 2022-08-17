e98990f2a33da557393fb5956d93df9358f72c46
OryChRamirez <105568434+OryChRamirez@users.noreply.github.com>
2022-08-16 22:03:47 -0500

    primer commit de la rama para lectura de extensión de archivos

--- index.js
+++ index.js
@@ -1,3 +1,37 @@
-module.exports = () => {
-  //se cambió el codigo?
-};
+const fs = require('fs');
+const path = require('path');
+
+function markdown() {
+  const testPath = './index.js';
+  // fs.readdir('./', (error, files) => {
+  //   if (error) {
+  //     throw error;
+  //   }
+  //   console.log(files);
+  // });
+
+  // VALIDACIÓN DE LA RUTA
+  const ExistPath = fs.existsSync(testPath);
+  console.log(ExistPath);
+  if (ExistPath) {
+  // SI EL ARCHIVO NO ES .MD RETORNA MSJ EN CONSOLA
+    const extension = path.extname(testPath);
+    if (extension === '.md') {
+      fs.readFile(testPath, 'UTF-8', (error, contFile) => {
+        if (error) {
+          throw error;
+        }
+        console.log('CONTENIDO: ', contFile);
+      });
+      console.log(extension);
+    } else {
+      console.log('El archivo no tiene una extensión markdown');
+    }
+  } else {
+    console.log('LA RUTA NO EXISTE');
+  }
+}
+
+markdown();
+
+// /\[([^\[]+)\](\(.*\))/gm     ------ regex
