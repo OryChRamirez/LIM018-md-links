168e8fed0c45adac56e3bb6d472ccae329f1cc18
OryChRamirez <105568434+OryChRamirez@users.noreply.github.com>
2022-08-16 22:59:24 -0500

    se verifica si la ruta existe y se convierte en absoluta.

--- index.js
+++ index.js
@@ -2,31 +2,19 @@ const fs = require('fs');
 const path = require('path');
 
 function markdown() {
-  const testPath = './index.js';
-  // fs.readdir('./', (error, files) => {
-  //   if (error) {
-  //     throw error;
-  //   }
-  //   console.log(files);
-  // });
+  const testPath = './prueba.md';
 
-  // VALIDACIÓN DE LA RUTA
+  // VALIDACIÓN DE LA EXISTENCIA DE LA RUTA
+
   const ExistPath = fs.existsSync(testPath);
   console.log(ExistPath);
   if (ExistPath) {
-  // SI EL ARCHIVO NO ES .MD RETORNA MSJ EN CONSOLA
-    const extension = path.extname(testPath);
-    if (extension === '.md') {
-      fs.readFile(testPath, 'UTF-8', (error, contFile) => {
-        if (error) {
-          throw error;
-        }
-        console.log('CONTENIDO: ', contFile);
-      });
-      console.log(extension);
-    } else {
-      console.log('El archivo no tiene una extensión markdown');
-    }
+    // CONVERTIR LA RUTA DE RELATIVA A ABSOLUTA
+
+    const convertToAbsolute = (pathEntered) => (
+      path.isAbsolute(pathEntered) ? pathEntered : path.resolve(pathEntered));
+    console.log(convertToAbsolute(testPath));
+    console.log('LA RUTA EXISTE');
   } else {
     console.log('LA RUTA NO EXISTE');
   }
@@ -34,4 +22,23 @@ function markdown() {
 
 markdown();
 
+// fs.readdir('./', (error, files) => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log(files);
+// });
+// SI EL ARCHIVO NO ES .MD RETORNA MSJ EN CONSOLA
+// const extension = path.extname(testPath);
+// if (extension === '.md') {
+//   fs.readFile(testPath, 'UTF-8', (error, contFile) => {
+//     if (error) {
+//       throw error;
+//     }
+//     console.log('CONTENIDO: ', contFile);
+//   });
+//   console.log(extension);
+// } else {
+//   console.log('El archivo no tiene una extensión markdown');
+// }
 // /\[([^\[]+)\](\(.*\))/gm     ------ regex
